dc9208e5a48c8fa565163060dfab9f4cc1b82c21
Matt Tyson <mtyson@mtyson.id.au>
2010-07-25 04:19:07 +0000

    More work on the ship fitter

---
+++ src/Core/SQLite Database/CCP Database/CCPEffectData.h
@@ -0,0 +1,17 @@
+//
+//  CCPEffectData.h
+//  Mac Eve Tools
+//
+//  Created by Matt Tyson on 25/07/10.
+//  Copyright 2010 Sebastian Kruemling. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface CCPEffectData : NSObject {
+	NSInteger effectID;
+	NSString *effectName;
+}
+
+@end
---
+++ src/Core/SQLite Database/CCP Database/CCPEffectData.m
@@ -0,0 +1,14 @@
+//
+//  CCPEffectData.m
+//  Mac Eve Tools
+//
+//  Created by Matt Tyson on 25/07/10.
+//  Copyright 2010 Sebastian Kruemling. All rights reserved.
+//
+
+#import "CCPEffectData.h"
+
+
+@implementation CCPEffectData
+
+@end
